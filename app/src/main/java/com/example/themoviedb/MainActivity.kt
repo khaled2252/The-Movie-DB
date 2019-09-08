f1f5dfe322950daec68d49184a4158e84a78c25c
raghda.hany <raghda.hany@ibtikar.net.sa>
2019-09-08 17:08:11 +0200

    set max lines for edittext

--- app/src/main/java/com/example/themoviedb/MainActivity.kt
+++ app/src/main/java/com/example/themoviedb/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.themoviedb
 
+import android.content.Context
 import android.graphics.Color
 import android.os.AsyncTask
 import android.os.Bundle
@@ -9,6 +10,8 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import com.example.themoviedb.pojos.KnownFor
