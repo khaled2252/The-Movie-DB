9d1f9b0b9cc3b3773e0764d93c75585c1da28737
Khaled Ahmed <khaled.ahmed@ibtikar.net.sa>
2019-09-23 10:18:44 +0200

    Remove SearchFlag tests Rename SearchResults to SearchTests Add mock-maker-inline

--- app/src/test/java/com/example/themoviedb/SearchTests.kt
+++ app/src/test/java/com/example/themoviedb/SearchTests.kt
@@ -1,35 +1,58 @@
 package com.example.themoviedb
 
+import android.content.Context
 import com.example.themoviedb.screens.main.Contract
 import com.example.themoviedb.screens.main.MainPresenter
-import com.nhaarman.mockito_kotlin.any
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.never
-import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.*
 import org.junit.Before
 import org.junit.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+
 
 class SearchTests {
     private lateinit var presenter: MainPresenter
     private lateinit var view: Contract.MainView
     private lateinit var model: Contract.MainModel
 
+    @Mock
+    private lateinit var mockApplicationContext: Context
+
     @Before
     fun setup() {
         view = mock()
         model = mock()
         presenter = MainPresenter(view, model)
+        MockitoAnnotations.initMocks(this) //To init context
     }
 
     @Test
-    fun search_with_non_empty_string_callsSetSearchFlag() {
-        presenter.searchOnClicked("jason")
-        verify(view).setSearchFlag(any())
+    fun search_non_empty_string_callsFetchJson() {
+        val currentPage = 1
+        val query = "jason"
+
+        presenter.searchOnClicked(query)
+        `when`(mockApplicationContext.getSystemService(Context.INPUT_METHOD_SERVICE)).thenReturn("")
+
+        verify(model).fetchJson(
+            eq(currentPage),
+            eq(query),
+            any()
+        )
     }
 
     @Test
-    fun search_with_empty_string_never_callsSetSearchFlag() {
-        presenter.searchOnClicked("")
-        verify(view, never()).setSearchFlag(any())
+    fun search_empty_string_never_callsFetchJson() {
+        val query = ""
+
+        presenter.searchOnClicked(query)
+        `when`(mockApplicationContext.getSystemService(Context.INPUT_METHOD_SERVICE)).thenReturn("")
+        verify(model, never()).fetchJson(
+            ArgumentMatchers.anyInt(),
+            ArgumentMatchers.anyString(),
+            any()
+        )
     }
 }
